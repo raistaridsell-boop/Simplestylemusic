baa9f8df0c99412e47368d21a149ad8ec926c266
raistaridsell-boop <raistaridsell@gmail.com>
2026-05-29 18:08:41 +0530

    Update project name and maintainer information

--- SIMPLE_MUSIC/core/bot.py
+++ SIMPLE_MUSIC/core/bot.py
@@ -1,6 +1,6 @@
 # -----------------------------------------------
-# 🔸 SIMPLE MUSIC Project
-# 🔹 Developed & Maintained by: Simple Boy (https://github.com/Simple-Boy-1k)
+# 🔸 KRISH MUSIC Project
+# 🔹 Developed & Maintained by: Simple Boy (https://github.com/raistaridsell-boop)
 # 📅 Copyright © 2026 – All Rights Reserved
 #
 # 📖 License:
@@ -9,7 +9,7 @@
 # Commercial use, redistribution, or removal of this notice is strictly prohibited
 # without prior written permission from the author.
 #
-# ❤️ Made with dedication and love by Simple_Boy_1k
+# ❤️ Made with dedication and love by raistaridsell-boop 
 # -----------------------------------------------
 from pyrogram import Client, errors
 from pyrogram.enums import ChatMemberStatus, ParseMode
