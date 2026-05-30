41cfd82a0904f30e55fb0cd6981f4b75a8bd763b
raistaridsell-boop <raistaridsell@gmail.com>
2026-05-30 10:57:39 +0530

    Update project name and author information in promote.py

--- SIMPLE_MUSIC/plugins/admins/promote.py
+++ SIMPLE_MUSIC/plugins/admins/promote.py
@@ -1,6 +1,6 @@
 # -----------------------------------------------
-# 🔸 SIMPLE MUSIC Project
-# 🔹 Developed & Maintained by: Simple Boy (https://github.com/Simple-Boy-1k)
+# 🔸 FERRARI MUSIC Project
+# 🔹 Developed & Maintained by: KRISHNXT (https://github.com/raistaridsell-boop)
 # 📅 Copyright © 2026 – All Rights Reserved
 #
 # 📖 License:
@@ -9,13 +9,13 @@
 # Commercial use, redistribution, or removal of this notice is strictly prohibited
 # without prior written permission from the author.
 #
-# ❤️ Made with dedication and love by Simple_Boy_1k
+# ❤️ Made with dedication and love by raistaridsell-boop 
 # -----------------------------------------------
 from pyrogram import Client, filters, enums
 from pyrogram.types import ChatPrivileges
 from pyrogram.errors import ChatAdminRequired
 from functools import wraps
-from SIMPLE_MUSIC import app
+from KRISH_MUSIC import app
 
 def mention(user_id, name):
     return f"[{name}](tg://user?id={user_id})"
