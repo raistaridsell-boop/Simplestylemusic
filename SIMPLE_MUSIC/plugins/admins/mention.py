c57be01e84d237d1677719c56eaa12f177e7fe6d
raistaridsell-boop <raistaridsell@gmail.com>
2026-05-30 10:55:37 +0530

    Update project name and maintainer information

--- SIMPLE_MUSIC/plugins/admins/mention.py
+++ SIMPLE_MUSIC/plugins/admins/mention.py
@@ -1,6 +1,6 @@
 # -----------------------------------------------
-# 🔸 SIMPLE MUSIC Project
-# 🔹 Developed & Maintained by: Simple Boy (https://github.com/Simple-Boy-1k)
+# 🔸 FERRARI MUSIC Project
+# 🔹 Developed & Maintained by: KRISHNXT (https://github.com/raistaridsell-boop)
 # 📅 Copyright © 2026 – All Rights Reserved
 #
 # 📖 License:
@@ -9,13 +9,13 @@
 # Commercial use, redistribution, or removal of this notice is strictly prohibited
 # without prior written permission from the author.
 #
-# ❤️ Made with dedication and love by Simple_Boy_1k
+# ❤️ Made with dedication and love by raistaridsell-boop 
 # -----------------------------------------------
 import asyncio
 from pyrogram.enums import ChatType, ChatMemberStatus
-from SIMPLE_MUSIC import app
+from KRISH_MUSIC import app
 from pyrogram import filters
-from SIMPLE_MUSIC.utils.Simple_ban import admin_filter
+from KRISH_MUSIC.utils.Simple_ban import admin_filter
 
 SPAM_CHATS = []
 
